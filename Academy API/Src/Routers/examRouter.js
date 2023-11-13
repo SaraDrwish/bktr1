8983075de3882ee51ef0f01ef117fdabd3b51dc1
Sara Drwish <38770404+SaraDrwish@users.noreply.github.com>
2023-11-14 01:33:42 +0300

    2

--- Academy API/Src/Routers/examRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const examController = require('../Controllers/examController');
-
-router.post('/exams', examController.createExam);
- 
-module.exports = router;
